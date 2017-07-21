3a6d14d533ba33d94eadf34f15633ea1a57af477
Kotaro Ashizawa <k-ashizawa@hykwlab.org>
2017-07-21 03:45:30 +0000

    Change the setup way
    Since the setup way with cron is not standard, Makefile is prepared.
    Moreover , the setup script is placed to /etc/init.d.

--- setup2.bash
+++ setup2.bash
@@ -1,4 +1,12 @@
 #!/bin/bash -xve
+### BEGIN INIT INFO
+# Provides:		raspimouse
+# Required-Start:	$local_fs
+# Required-Stop:	$local_fs
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	raspimouse
+### END INIT INFO
 
 exec 2> /tmp/setup.log
 
@@ -9,3 +17,4 @@ sleep 1
 chmod 666 /dev/rt*
 
 echo 0 > /dev/rtmotoren0
+
